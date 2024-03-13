20b04510b9b74d5503c275c8c58262adfc002683
Rohitilak <rohittilak086@gmail.com>
2024-03-14 03:31:39 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Progressbar from './ProgressBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Progressbar/>
+    </div>
+  );
+}
+
+export default App;
